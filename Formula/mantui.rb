befd83f39217514bdc5c580002ec633fff2f124c
Philipp Reiter <p.reiter93@gmail.con>
2025-01-29 21:36:33 +0100

    Update shasum

--- Formula/mantui.rb
+++ Formula/mantui.rb
@@ -8,20 +8,20 @@ class Mantui < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/preiter93/mantui/releases/download/v0.1.0/mantui-0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "3b21fbd391480b7b889bc0a9d71ab0966b5177792f6d45e6276047f0c5d565a6"
+      sha256 "fccf386c053c2c5760f7755bc198268db6f33b5f1b3691b785acd1cbd86e874e"
     else
       rl "https://github.com/preiter93/mantui/releases/download/v0.1.0/mantui-0.1.0-x86_64-apple-darwin.tar.gz"
-      sha256 "2498bf1fa6c6efba117fd11cbc16777411c4a2f1358531ec51e23ecbc97aafa5"
+      sha256 "e366f521c2a1a7e67cd6c33dc7bb9ea5c09a76d16bc1b2b5c6e38244153566bd"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/preiter93/mantui/releases/download/v0.1.0/mantui-0.1.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "ff2b8603de713d2dde2a80e922db86e775821c9447986b58f75687e02c33e16b"
+      sha256 "dc87a3de680e19071abf46e52f4302bb076623afd42d1ca53dc53e39b1bfa786"
     else
       url "https://github.com/preiter93/mantui/releases/download/v0.1.0/mantui-0.1.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "8e668091b312a08fdf1aa6bcbbd9694118adc5a5da04d532624cfd467d42b333"
+      sha256 "a93e8b517c2ff0d6c8f25fc8864df4eb628317450b70cf8a2147bfc11349e1bd"
     end
   end
   
